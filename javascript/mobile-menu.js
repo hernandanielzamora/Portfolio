34ef2f91cadf86ea1829e62f170848c4f8a610fa
Hernan Zamora <hernanzamora1002@gmail.com>
2023-03-06 14:54:35 -0300

    Created and finished mobile menu using JS

---
+++ javascript/mobile-menu.js
@@ -0,0 +1,16 @@
+/* eslint linebreak-style: ["error", "windows"] */
+const getMenuBtn = document.getElementById('ham-menu');
+const getNavBar = document.querySelector('.nav-sidebar');
+const getNavLogg = document.querySelector('.logo');
+
+getMenuBtn.addEventListener('click', () => {
+  getMenuBtn.classList.toggle('active');
+  getNavBar.classList.toggle('active');
+  getNavLogg.classList.toggle('active');
+});
+
+document.querySelectorAll('.nave-link').forEach((n) => n.addEventListener('click', () => {
+  getMenuBtn.classList.remove('active');
+  getNavBar.classList.remove('active');
+  getNavLogg.classList.remove('active');
+}));
